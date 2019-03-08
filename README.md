88b483b6d546b60546a00e635cbe74fd1f1a5f31
orkunkhrmn <orkun-kahraman@hotmail.com>
2019-03-08 15:18:10 +0300

    örnek

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # nlog
 nlog example
+
+
+````
+
+PM> Install-Package NLog
+PM> Install-Package NLog.Config
+
+````
+
+> [Kaynak](https://medium.com/@adewagold_35293/c-logging-files-with-nlog-6d4fe2f11a58)
---
+++ ngloexample/ngloexample/Program.cs
@@ -0,0 +1,44 @@
+﻿using NLog;
+using System;
+using System.IO;
+
+namespace ngloexample
+{
+    internal class Program
+    {
+        public static Logger logger = LogManager.GetCurrentClassLogger();
+
+        private static void Main(string[] args)
+        {
+            logger.Trace("This is app has been launched");
+            ReadFile();
+        }
+
+        private static void ReadFile()
+        {
+            try
+            {
+                using (var fileReader = new StreamReader(@"/Users/test"))
+                {
+                    var textFile = fileReader.ReadToEnd();
+                    Console.WriteLine(textFile);
+                }
+            }
+            catch (DriveNotFoundException ex)
+            {
+                Console.WriteLine("Klasor bulunamadı");
+                logger.Error("Klasor bulunmadı" + ex.Message);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Dosya bulunamadı");
+                logger.Error("Dosya bulunmadı" + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Dosya yeri bulunamadı");
+                logger.Error("Dosya yeri bulunmadı" + ex.Message);
+            }
+        }
+    }
+}
